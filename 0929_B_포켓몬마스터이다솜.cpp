908b97294bb5cd98e04b86de3502dc60bc7bd7d6
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-10-01 23:38:00 +0900

    map 공부 중

--- 0929_B_포켓몬마스터이다솜.cpp
+++ 0929_B_포켓몬마스터이다솜.cpp
@@ -1,8 +1,30 @@
 #include <iostream>
 #include <map>
+#include <vector>
+#include <string>
 using namespace std;
 
+#define endl "\n"
 int main() {
-	map<string, int> m;
-	m.
+	int n, m;
+	cin >> n >> m;
+	map<int, string> map1;
+	map<string, int> map2;
+	vector<string> result;
+
+	for (int i = 0; i < n; i++)
+	{
+		string str;
+		cin >> str;
+		map1.insert(make_pair(i + 1, str));
+		map2.insert(make_pair(str, i + 1));
+	}
+	for (int i = 0; i < m; i++)
+	{
+		string input;
+		cin >> input;
+		if (input[0] > '0' || input[0] < '9') {
+			map1.find(stoi(input));
+		}
+	}
 }
