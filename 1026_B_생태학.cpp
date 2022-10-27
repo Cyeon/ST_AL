90687030e5b1247c36d852c07729c373952e1267
Cyeon <77601035+Cyeon@users.noreply.github.com>
2022-10-27 18:00:08 +0900

    10/26 1문제 완료
    생태학 문제 완료. (map을 이용한 문제)

--- 1026_B_생태학.cpp
+++ 1026_B_생태학.cpp
@@ -1,7 +1,54 @@
 #include <iostream>
 #include <map>
+#include <string>
 using namespace std;
+
+#define endl "\n"
+
 int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(NULL);
+	cout.tie(NULL);
 
+	map<string, int> map1;
+	map<string, int>::iterator iter;
 
+	float allSize = 0;
+
+	while (true)
+	{
+		string str;
+		getline(cin, str);
+
+		iter = map1.find(str);
+
+		if (cin.eof() == true) {
+			break;
+		}
+
+		if (iter != map1.end()) {
+			int temp = iter->second;
+			temp++;
+			map1.erase(str);
+			map1.insert(make_pair(str, temp));
+		}
+		else
+		{
+			map1.insert(make_pair(str, 1));
+		}
+		allSize++;
+
+	}
+
+	cout << fixed;
+	cout.precision(4);
+
+	while (!map1.empty())
+	{
+		iter = map1.begin();
+		float result = iter->second * 100 / allSize;
+
+		cout << iter->first << " " << result << endl;
+		map1.erase(iter);
+	}
 }
